3eb461a8c06d21a9265d954de327d4cccce9bb27
Sudip Kumar Prasadmkdir <sudipkumarprasad2005@gmail.com>
2026-02-05 13:49:13 +0530

    CORS update

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'https://health-guide-8kzu.vercel.app'],
+    origin: process.env.FRONTEND_URL || ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'https://health-guide-8kzu.vercel.app'],
     credentials: true
 }));
 
